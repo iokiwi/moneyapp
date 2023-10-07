ff65fd304dcd7b0d24520726c5c483b6d6e88558
Simon Merrick <s.m3rrick@gmail.com>
2023-10-07 22:54:50 +1300

    Add error handling to sign up and sign in views

--- app/users/forms.py
+++ app/users/forms.py
@@ -4,11 +4,6 @@ from django import forms
 
 
 class EmailLoginForm(forms.Form):
-    # Note(simonm): I don't like that this is hardcoded. However, using 'reverse'
-    # here seems to result in a circular import.
-
-    success_url = "/accounts/pending"
-
     email = forms.EmailField(
         widget=forms.EmailInput(
             attrs={"class": "form-control", "placeholder": "Email Address"}
@@ -17,9 +12,6 @@ class EmailLoginForm(forms.Form):
 
 
 class UserRegistrationForm(forms.Form):
-    # Note(simonm): I don't like that this is hardcoded
-    success_url = "/accounts/pending"
-
     name = forms.CharField(
         max_length=100,
         widget=forms.TextInput(
--- app/users/views.py
+++ app/users/views.py
@@ -1,13 +1,19 @@
+from urllib.parse import urlencode
+
 from django.shortcuts import render, redirect
 from django.views.generic import FormView, TemplateView
 from django.urls import reverse
 from django.contrib.auth import get_user_model, logout
-from urllib.parse import urlencode
 from django.views import View
+from django.db import IntegrityError
+from django.conf import settings
 
 from sesame.utils import get_parameters
 
 from .forms import EmailLoginForm, UserRegistrationForm
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def get_user(email):
@@ -53,16 +59,22 @@ class EmailLoginView(FormView):
         user = get_user(email)
 
         if user is None:
-            # Ignore the case when no user is registered with this address.
-            # Possible improvement: send an email telling them to register.
-            print("user not found:", email)
-            return
+            logger.info("User not found: %s", email)
+            return render(self.request, "users/pending_validation.html")
 
         link = get_magic_link(self.request, user)
         send_email(user, link)
+
+        # If we are in debug mode, show the URL on the screen for convenience
+        context = {}
+        if settings.DEBUG is True and not settings.ENVIRONMENT.lower().startswith(
+            "prod"
+        ):
+            context["magic_link"] = link
+
         # NOTE(simonm): We currently do not do a redirect. Resubmitting the form
         # here will result in the post being sent again.
-        return render(self.request, "users/pending_validation.html")
+        return render(self.request, "users/pending_validation.html", context=context)
 
 
 class UserRegistrationFormView(FormView):
@@ -73,15 +85,32 @@ class UserRegistrationFormView(FormView):
     def form_valid(self, form):
         email = form.cleaned_data["email"]
 
-        # Create the user if they don't already exist.
-        User = get_user_model()
-        user = User(email=email, username=email)
-        user.set_unusable_password()
-        user.save()
+        try:
+            User = get_user_model()
+            user = User(email=email, username=email)
+            user.set_unusable_password()
+            user.save()
+        except IntegrityError as e:
+            # TODO: Ensure exception gets added to trace
+            logger.exception(e)
+            # Pretend the form was valid. We don't want to enable enumeration
+            # of user accounts.
+            return super().form_valid(form)
 
         link = get_magic_link(self.request, user)
         send_email(user, link)
-        return super().form_valid(form)
+
+        context = {}
+        if settings.DEBUG is True and not settings.ENVIRONMENT.lower().startswith(
+            "prod"
+        ):
+            context["magic_link"] = link
+
+        super().form_valid(form)
+
+        # NOTE(simonm): We currently do not do a redirect. Resubmitting the form
+        # here will result in the post being sent again.
+        return render(self.request, "users/pending_validation.html", context=context)
 
 
 class UserLogoutView(View):
